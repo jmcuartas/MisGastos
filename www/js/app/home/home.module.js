e3f203f8256ca6f30c6efdbeeb276bacba21837d
Juan Manuel Cuartas <mr_jackkas@MacBook-Pro-de-Juan.local>
2017-12-23 22:41:30 +0100

    feat(core-home): Added Core home

---
+++ www/js/app/home/home.module.js
@@ -0,0 +1,23 @@
+(function () {
+  'use strict';
+
+  angular
+    .module('app.home', [])
+    .config(config);
+
+  function config($stateProvider, $urlRouterProvider) {
+    $stateProvider
+
+    .state('app.home', {
+      url: '/home',
+      cache: false,
+      views: {
+        content: {
+          templateUrl: 'js/app/home/home.html',
+          controller: 'HomeController',
+          controllerAs: 'vm'
+        },
+      },
+    })
+  }
+})();
