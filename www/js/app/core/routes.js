ad0475a9b9cae1f74f78eda42a3e0a4b0ab33f85
Juan Manuel Cuartas <mr_jackkas@MacBook-Pro-de-Juan.local>
2017-12-23 21:33:50 +0100

    Create Core of app

---
+++ www/js/app/core/routes.js
@@ -0,0 +1,11 @@
+(function () {
+  'use strict';
+
+  var core = angular.module('app.core');
+  core.config(config);
+
+  /* @ngInject */
+  function config($stateProvider, $urlRouterProvider) {
+
+  }
+})();
