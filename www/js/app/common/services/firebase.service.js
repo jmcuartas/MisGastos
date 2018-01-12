af1569a6cc30b4eda9a721383735b8f6f5ece0ca
Juan Manuel Cuartas <juacuaza@MacBook-Pro-de-Juan.local>
2018-01-12 20:31:46 +0100

    delete(file-transfer)

--- www/js/app/common/services/firebase.service.js
+++ www/js/app/common/services/firebase.service.js
@@ -55,31 +55,9 @@
     var storage = firebase.storage();
     var storageRef = storage.ref();
     var fileRef = storageRef.child(path);
-    var storeFile = ( typeof cordova !== "undefined" )? cordova.file.dataDirectory : "";
-
     fileRef.getDownloadURL().then(function (url) {
-      //window.open(url)
-      downloadFile(url, storeFile + 'document.pdf');
+      window.open(url);
     });
-  }
-
-  function downloadFile(url, targetPath) {
-    var fileTransfer = new FileTransfer();
-    var trustHosts = true;
-    var options = {};
-
-    fileTransfer.download(
-      url,
-      targetPath,
-      function (entry) {
-        cordova.plugins.FileOpener.openFile(entry.toURL(), onSucces, onError);
-      },
-      function (error) {
-        onError(error);
-      },
-      trustHosts,
-      options
-    );
   }
 
   function signInWithEmailAndPassword(cred) {
